0e83c55a71efa5c8a6603dde1952aec2cad73892
root <zbb775885@163.com>
2016-06-13 02:00:11 -0400

    增加circle文件

---
+++ circle.py
@@ -0,0 +1,75 @@
+#!/usr/bin/python
+#-*- coding:utf-8 -*-
+
+
+
+###################循环语句for#######################
+names = ['bob', 'james', 'paul']
+for name in names:
+    print(name)
+##################################################
+
+
+
+#####################范围函数range################################
+#range(100)可产生0-99这100个数据
+sum = 0
+tmp = range(100)
+for i in tmp:
+    sum += i
+print("sum val:%d" %sum)
+#list函数可将列表中的数据打印出来
+print(list(tmp))
+####################################################
+
+
+###################循环语句while#######################
+#用法
+sum = 0
+n = 99
+while n > 0:
+    sum += n
+    n -= 1
+print("sum val:%d" %sum)
+
+
+
+
+
+
+
+var = input("please input any key to ending")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- judge.py
+++ judge.py
@@ -6,15 +6,23 @@
 age = 20
 if age > 10:
 	print('your age is %d' %age)
-	
+elif age > 5:
+    print("age is between 5-10")
+else:
+    print("age is too small")
 
 ##########################################
 
+###################input介绍#######################
+#input返回的数据时str类型，不能用于与整数比较大小等操作，但是可以用int来转换
+tmp = input("please input num:")
+tmp = int(tmp)
 
+print("val:%d" %tmp)
+
+##########################################
 
 var = input("please input any key to ending")
-
-
 
 
 

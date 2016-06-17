f31194fa6f9a98cd094298ae1a404861d2a69c09
zbb775885 <zbb775885@163.com>
2016-06-17 23:48:54 +0800

    增加函数定义及函数参数相关使用方法

--- func.py
+++ func.py
@@ -29,8 +29,7 @@ def move(x, y , step, angle= 0):
 x, y = move(100, 20, 60, math.pi / 6)#多值返回其实返回的是一个tuple，多变量接受时按顺序接受，如果写一个值则返回一个元组值    
 print(x, y)
 
-var = input("please input any key to ending")
-
+#var = input("please input any key to enter function's parameter")
 #定义函数时，需要确定函数名和参数个数；
 
 #如果有必要，可以先对参数的数据类型做检查；
@@ -46,12 +45,83 @@ var = input("please input any key to ending")
 
 
 #########################函数参数#################################################
+#位置参数
+#添加默认参数，可以不用输入y。且默认参数必须在非默认参数后，或者都没有
+def my_pow(x = 3, y = 2): 
+    tmp = 1
+    while y > 0:
+        tmp = tmp * (x)
+        y -= 1
+    return tmp
+
+print("val:%d" %(my_pow(5,4)))
+
+print("val:%d" %(my_pow()))
+#默认参数好的好处就是可以降低函数调用的复杂度。
+#一年级小学生注册的函数，需要传入name和gender，但如果要继续传入年龄等会增加复杂度，设置默认参数后可不用传入参数即可解决
+def enroll(name, gender, age=6, city='Beijing'):
+    print('name:', name)
+    print('gender:', gender)
+    print('age:', age)
+    print('city:', city)
+#如上，如需不按顺序提供默认参数，则需要将默认参数名写上。 
+enroll("12", "fds", city='Beijing')
+#需要记住默认参数必须指向不变参数，如list参数不建议使用
+def add_end(l = []):#l的指针指向[]，如果[]增加了参数则l指向的值会变化
+    l.append('end')
+    return l
+print(add_end())
+print(add_end())
+print(add_end())
+#如要解决该问题可如下修改
+def add_end1(l = None):
+    if l == None:
+        l = []
+    l.append('end')
+    return l
+print(add_end1())
+print(add_end1())
+print(add_end1())
 
 
+#############################可变参数##########################################
+#可变参数会把传入的参数元组（tuple）化，定义方式为在参数前增加*，如下
+def my_sum(*num):
+    sum = 0
+    for i in num:
+        sum += i
+    return sum
+print("my_sum:%d" %(my_sum(10,12,343)))
+#可以把传入的参数可变参数化
+data = (10, 12, 343)
+print("variable sum:%d" %(my_sum(*data)))
+
+#############################关键字参数##########################################
+#关键字参数会把传入的参数字典化dict,定义方式为在参数前增加**,关键字可为0个或任意个
+def person(name, age, **kw):
+    print('name' , name, 'age', age, 'kw:' , kw)
+
+person('Adam', 45, gender='M', job='Engineer')
+
+#和可变参数类似，也可以先组装出一个dict，然后，把该dict转换为关键字参数传进去
+extra = {'city': 'Beijing', 'job': 'Engineer'}
+person('Adam', 45, **extra)
+#**extra表示把extra这个dict的所有key-value用关键字参数传入到函数的**kw参数，kw将获得一个dict，
+#注意kw获得的dict是extra的一份拷贝，对kw的改动不会影响到函数外的extra
 
 
+############################命名关键字参数############################################################
+#跟字典类似，但是必须有名字，定义方法如下
+def func1(a, b , *, test1, test2):
+    print(a, b, test1, test2)
+func1(1, 2 ,test1 = 3, test2= 4)#调用时必须写上test1=... test2=...
 
+def func2(a, b , *args, test1, test2):#如果定义时有可变参数则命名关键字的*可不需要
+    print(a, b, args, test1, test2)
+func2(1, 2 ,45, 4543, 453, test1 = 3, test2= 4)#调用时必须写上test1=... test2=...
 
+#############################################参数组合#################################
+input("please input any key to ending")
 
 
 
